2e1327df842b16f570268c8748cb1525ee8d382f
unknown <joaoferreira6173@digitalcollege.internal>
2025-08-25 20:39:37 -0300

    disparando emails usando sendgrid

--- index.js
+++ index.js
@@ -1,39 +1,63 @@
-const express = require('express')
-const cors = require('cors');
+const email = require("@sendgrid/mail");
+const express = require("express");
+const cors = require("cors");
+const { Prisma } = require("@prisma/client");
 
-const app = express()
-const port = 3001
+email.setApiKey(
+  "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"
+);
+
+const { PrismaClient } = require("./generated/prisma");
+
+const prisma = new PrismaClient();
+
+const app = express();
+const port = 3001;
 
 app.use(cors());
 app.use(express.json());
 
-let cars = []
+let cars = [];
 
+app.get("/cars", (req, res) => {
+  //   ira retornar uma lista de carros com status 200
 
-app.get('/cars', (req, res) => {
-    //   ira retornar uma lista de carros com status 200
+  res.status(200).json(cars);
+});
 
-    res.status(200).json(cars)
-})
+app.get("/send-email", async (req, res) => {
+  const emailContent = {
+    to: "jpedromaciel25@gmail.com",
+    from: "jpedromaciel25@gmail.com",
+    subject: "Sending an email using SendGrid",
+    text: "Hello from SendGrid!",
+  };
+  try {
+    await email.send(emailContent);
+    res.status(200).json({ message: "Email sent successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Error sending email" });
+  }
+});
 
-app.post('/cars', (req, res) => {
-    console.log(req.body)
-    const car = {
-        name: req.body.name,
-        category: req.body.category,
-        seats: req.body.seats,
-        price: req.body.price,
-        transmission: req.body.transmission,
-        fuel: req.body.fuel,
-        image: req.body.image,
-        available: req.body.available,
-    }
+app.post("/cars", (req, res) => {
+  console.log(req.body);
+  const car = {
+    name: req.body.name,
+    category: req.body.category,
+    seats: req.body.seats,
+    price: req.body.price,
+    transmission: req.body.transmission,
+    fuel: req.body.fuel,
+    image: req.body.image,
+    available: req.body.available,
+  };
 
-    //   ira adicionar um carro com status 201
-    cars.push(car)
-    res.status(201).json(car)
-})
+  //   ira adicionar um carro com status 201
+  cars.push(car);
+  res.status(201).json(car);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
